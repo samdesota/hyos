23ec5722ab809ef4093f139ffe830cae4ad86d92
Sam DeSota <me@samdesota.com>
2026-09-10 13:27:34 -0400

    Add keybinding remote capability (register/unregister, triggered event)

--- packages/hyos/capabilities/index.ts
+++ packages/hyos/capabilities/index.ts
@@ -1,15 +1,18 @@
 export * from "./browser.js";
 export * from "./agent.js";
 export * from "./ui-agent.js";
+export * from "./keybinding.js";
 
 import { browserCapability } from "./browser.js";
 import { agentCapability } from "./agent.js";
 import { uiAgentCapability } from "./ui-agent.js";
+import { keybindingCapability } from "./keybinding.js";
 import { reloadCapability } from "./reload.js";
 
 export const applicationCapabilities = [
   browserCapability,
   agentCapability,
   uiAgentCapability,
+  keybindingCapability,
   reloadCapability,
 ] as const;
